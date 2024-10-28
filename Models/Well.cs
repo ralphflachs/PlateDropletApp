199ccc673fd77e965f7fdb89402e9c85136c707c
Ralph Flachsbart <ralphflachs@gmail.com>
2024-10-28 16:37:32 -0700

    Well properties

--- Models/Well.cs
+++ Models/Well.cs
@@ -2,7 +2,38 @@
 
 namespace PlateDropletApp.Models
 {
-    public class Well
+    public class Well : BindableBase
     {
+        private int _dropletCount;
+        private bool _isLowDroplet;
+        private string _displayText;
+        private string _backgroundColor;
+
+        public int WellIndex { get; set; }
+        public string WellName { get; set; }
+
+        public int DropletCount
+        {
+            get => _dropletCount;
+            set => SetProperty(ref _dropletCount, value);
+        }
+
+        public bool IsLowDroplet
+        {
+            get => _isLowDroplet;
+            set => SetProperty(ref _isLowDroplet, value);
+        }
+
+        public string DisplayText
+        {
+            get => _displayText;
+            set => SetProperty(ref _displayText, value);
+        }
+
+        public string BackgroundColor
+        {
+            get => _backgroundColor;
+            set => SetProperty(ref _backgroundColor, value);
+        }
     }
 }
