10afa7658dd9ae6235dcf3eec25d8495f8bf24e8
Ralph Flachsbart <ralphflachs@gmail.com>
2024-10-28 16:29:57 -0700

    App.xml: Set startup URI Main Window View: Bind controls, summary, plate grid

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -9,11 +9,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace PlateDropletApp
+namespace PlateDropletApp.Views
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
