ab8719a06757957a4245ae326c9aded59e439afc
Ralph Flachsbart <ralphflachs@gmail.com>
2024-10-28 15:47:15 -0700

    MainWindow VM: core properties, placeholders for handles. Plate Model: row and column count. Well Model: Placeholder

---
+++ Models/Plate.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.ObjectModel;
+
+namespace PlateDropletApp.Models
+{
+    public class Plate
+    {
+        public ObservableCollection<Well> Wells { get; set; } = new ObservableCollection<Well>();
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+    }
+}
---
+++ Models/Well.cs
@@ -0,0 +1,8 @@
+﻿using Prism.Mvvm;
+
+namespace PlateDropletApp.Models
+{
+    public class Well
+    {
+    }
+}
