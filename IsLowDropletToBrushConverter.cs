82915eebd823ad81736cbe6baa80b93c37ea3b74
Ralph Flachsbart <ralphflachs@gmail.com>
2024-10-28 20:12:14 -0700

    Use Value Converters: Use IValueConverter to convert IsLowDroplet to a Brush for background color.

---
+++ IsLowDropletToBrushConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace PlateDropletApp.Converters
+{
+    public class IsLowDropletToBrushConverter : IValueConverter
+    {
+        private static readonly Brush LowDropletBrush = Brushes.Gray;
+        private static readonly Brush NormalBrush = Brushes.White;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isLowDroplet = (bool)value;
+            return isLowDroplet ? LowDropletBrush : NormalBrush;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IsLowDropletToDisplayTextConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PlateDropletApp.Converters
+{
+    public class IsLowDropletToDisplayTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isLowDroplet = (bool)value;
+            return isLowDroplet ? "L" : "n";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
