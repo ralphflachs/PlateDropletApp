ca37f0a6e4fdf514f2371c27bc621a99b887b98d
Ralph Flachsbart <ralphflachs@gmail.com>
2024-10-29 15:28:07 -0700

    Error template to display validation errors. Bind controls to validation logic

---
+++ Converters/InverseBooleanConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PlateDropletApp.Converters
+{
+    public class InverseBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return !boolValue;
+            }
+            return true;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
