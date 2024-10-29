a76052c7bf221c835d5621adb43b843905110c97
Ralph Flachsbart <ralphflachs@gmail.com>
2024-10-28 20:12:51 -0700

    Dependency Injection in Views: Use a DI container to inject the ViewModel into the View.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,14 +1,35 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.DependencyInjection;
+using PlateDropletApp.Services;
+using PlateDropletApp.ViewModels;
+using PlateDropletApp.Views;
+using System;
 using System.Windows;
 
 namespace PlateDropletApp
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-    }
+        private ServiceProvider _serviceProvider;
 
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+
+            var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<PlateDataService>();
+            services.AddSingleton<IFileDialogService, FileDialogService>();
+            services.AddTransient<MainWindowViewModel>();
+            services.AddTransient<MainWindow>();
+        }
+    }
 }
